6069dd24b1eeb92dd9640b6bb7c5c268cb0ba0e6
aibarra-vegga <129413159+abdel1311fatah@users.noreply.github.com>
2023-10-11 11:11:32 +0200

    acabat

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-
-        System.out.println("1. Demanar dades del producte i insertar-les al fitxer, mantenint els valors anteriors.\n" +
-                "2. Mostrar el preu d'un determinat producte.\n" +
-                "3. Modificar el preu d'un determinat producte.\n" +
-                "4. Mostrar tots els productes.\n" +
-                "5. Esborrar un determinat producte.\n" +
-                "6. Sortir.");
-
         ArrayList<Producte> productes = new ArrayList<>();
         int opcio = 0;
 
@@ -24,6 +16,13 @@ public class Main {
         ObjectInputStream ois = new ObjectInputStream(fis);
 
         do {
+            System.out.println("\n1. Demanar dades del producte i insertar-les al fitxer, mantenint els valors anteriors.\n" +
+                    "2. Mostrar el preu d'un determinat producte.\n" +
+                    "3. Modificar el preu d'un determinat producte.\n" +
+                    "4. Mostrar tots els productes.\n" +
+                    "5. Esborrar un determinat producte.\n" +
+                    "6. Sortir.\n" +
+                    "7. Productes test");
             opcio = obtindreInt("Que vols fer? ");
             if (opcio == 1) {
                 int c1 = obtindreInt("Quants productes vols insertar? ");
@@ -37,97 +36,89 @@ public class Main {
                     System.out.println("--------------------------------------");
                 }
             } else if (opcio == 2) {
-
                 int c1 = obtindreInt("Id del producte que vols veure: ");
                 boolean trobat = false;
 
                 for (Producte producte : productes) {
-                    producte = (Producte) ois.readObject();
                     if (producte.getId() == c1) {
                         System.out.println("Producte amb el nom: " + producte.getNom() + " amb el id: " + producte.getId() + " : ");
                         System.out.println(producte.getPreu());
                         System.out.println("-----------------------------------------------------------------------------------");
                         trobat = true;
-                        break; // per a que no recorreixi tot el arraylist i nomes en printi un
+                        break;
                     }
                 }
 
                 if (!trobat) {
-                    System.out.println("No s ha trobat cap producte");
+                    System.out.println("No s'ha trobat cap producte");
                 }
-
             } else if (opcio == 3) {
-
-                int c1 = obtindreInt("Id del producte que vols cambiar el preu: ");
+                int c1 = obtindreInt("Id del producte que vols canviar el preu: ");
                 boolean trobat = false;
 
-                oos.close();
-
                 for (Producte producte : productes) {
-                    producte = (Producte) ois.readObject();
-                    if (producte.getId() == c1 && !trobat) {
+                    if (producte.getId() == c1) {
                         System.out.println("Producte amb el nom: " + producte.getNom() + " amb el id: " + producte.getId());
                         System.out.println("Preu vell: " + producte.getPreu());
                         System.out.println("-----------------------------------------------------------------------------------");
-                        producte.setPreu(obtindreDouble("Introdueix el nou preu del producte amb el id " + producte.getId()));
+                        double nouPreu = obtindreDouble("Introdueix el nou preu del producte amb el id " + producte.getId());
+                        producte.setPreu(nouPreu);
                         System.out.println("Preu nou: " + producte.getPreu());
                         oos.writeObject(producte);
-
                         trobat = true;
+                        break;
                     }
                 }
 
                 if (!trobat) {
-                    System.out.println("No s ha trobat cap producte");
+                    System.out.println("No s'ha trobat cap producte");
                 }
-
             } else if (opcio == 4) {
                 for (Producte producte : productes) {
-                    producte = (Producte) ois.readObject();
                     System.out.println(producte.toString());
                     System.out.println("------------------------------------------------------------------------------------");
                 }
             } else if (opcio == 5) {
-
                 int c1 = obtindreInt("Id del producte que vols borrar ");
                 boolean trobat = false;
-
+                ArrayList<Producte> productesTemp = new ArrayList<>();
                 for (Producte producte : productes) {
-                    producte = (Producte) ois.readObject();
-                    if (producte.getId() == c1 && !trobat) {
+                    if (producte.getId() == c1) {
                         System.out.println("Producte amb el nom: " + producte.getNom() + " amb el id: " + producte.getId() + " i preu: " + producte.getPreu());
                         System.out.println("-----------------------------------------------------------------------------------");
-                        producte.setPreu(null);
-                        producte.setId(-1);
-                        producte.setNom(null);
-                        productes.remove(producte);
-                        oos.writeObject(producte);
-
+                        productesTemp.add(producte);
                         trobat = true;
+                    } else {
+                        oos.writeObject(producte);
                     }
                 }
 
-                if (!trobat) {
-                    System.out.println("No s ha trobat cap producte");
+                if (trobat) {
+                    productes = productesTemp;
+                } else {
+                    System.out.println("No s'ha trobat cap producte");
                 }
-
             } else if (opcio == 6) {
                 System.out.println("Has sortit de la tenda");
             } else if (opcio == 7) {
                 Producte producte = new Producte();
                 producte.setPreu(43.0);
-                producte.setNom(("a"));
-                producte.setId((1));
+                producte.setNom("a");
+                producte.setId(1);
                 productes.add(producte);
                 oos.writeObject(producte);
                 Producte producte2 = new Producte();
                 producte2.setPreu(44.0);
-                producte2.setNom(("b"));
-                producte2.setId((2));
+                producte2.setNom("b");
+                producte2.setId(2);
                 productes.add(producte2);
                 oos.writeObject(producte2);
+                for (Producte p : productes) {
+                    System.out.println(p.toString());
+                    System.out.println("------------------------------------------------------------------------------------");
+                }
             } else {
-                System.out.println("Introdueix un número valid, ha de ser del 1 al 6");
+                System.out.println("Introdueix un número valid, ha de ser del 1 al 7");
             }
         } while (opcio != 6);
 
@@ -139,19 +130,8 @@ public class Main {
 
     public static String obtindreString(String text) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print(text);
         String text_usuari = sc.nextLine();
-
-        return text_usuari;
-    }
-
-    public static Character obtindreChar(String text) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print(text);
-        Character text_usuari = sc.nextLine().charAt(0);
-
         return text_usuari;
     }
 
@@ -165,7 +145,7 @@ public class Main {
             TipusCorrecte = sc.hasNextDouble();
             if (!TipusCorrecte) {
                 sc.nextLine();
-                System.out.println("Error Valor no valid");
+                System.out.println("Error: Valor no válido");
             } else {
                 num_usuari = sc.nextDouble();
             }
@@ -174,7 +154,6 @@ public class Main {
     }
 
     public static int obtindreInt(String text) {
-
         boolean TipusCorrecte;
         Scanner sc = new Scanner(System.in);
         int num_usuari = 0;
@@ -192,5 +171,4 @@ public class Main {
 
         return num_usuari;
     }
-
 }
